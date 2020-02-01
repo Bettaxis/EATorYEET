bbb4071bc045e81e7d3f05dc0fa359d1f2655197
Adrian Miasik <AdrianMiasik@gmail.com>
2020-01-31 22:26:28 -0500

    EOD game jam commit
    Eh, what did you expect :D

---
+++ Assets/Console.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour
+{
+    [SerializeField] private ConsoleLog consoleLogPrefab;
+    
+    [SerializeField] private List<ConsoleLog> logs = new List<ConsoleLog>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            CreateLog("I am a pizza");
+        }
+    }
+
+    private void CreateLog(string message)
+    {
+        ConsoleLog log = Instantiate(consoleLogPrefab, transform);
+        log.Initialize(this, message);
+        logs.Add(log);
+    }
+
+    public void RemoveLog(ConsoleLog _log)
+    {
+        logs.Remove(_log);
+        Destroy(_log.gameObject);
+    }
+}
--- Assets/ConsoleLog.cs
+++ Assets/ConsoleLog.cs
@@ -1,21 +1,43 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConsoleLog : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI text;
     [SerializeField] private float displayForHowLongInSeconds = 3f;
 
+    private Console console;
+    private bool isInit;
     private float accumulatedTime;
+
+    public void Initialize(Console _console, string message)
+    {
+        if (console == null)
+        {
+            Debug.LogWarning("Console log could not be created.");
+        }
+
+        console = _console;
+        text.text = message;
+        
+        isInit = true;
+    }
     
     private void Update()
     {
+        if (!isInit)
+        {
+            return;
+        }
+            
         accumulatedTime += Time.deltaTime;
 
         if (accumulatedTime >= displayForHowLongInSeconds)
         {
-            Destroy(gameObject);
+            console.RemoveLog(this);
         }
     }
 }
