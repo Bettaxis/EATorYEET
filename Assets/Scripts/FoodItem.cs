09406d0768097dea153c006fc4081b748022517c
KelvinPut <kelvin@stitchmedia.ca>
2020-02-01 12:41:53 -0500

    pre merge of SO branch

--- Assets/Scripts/FoodItem.cs
+++ Assets/Scripts/FoodItem.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FoodItem : MonoBehaviour
 {
+    //[SerializeField]
+    //private FoodItem
     public float speed = 0.25f;
 
     public float distanceFromPlayer = 0.3f; 
