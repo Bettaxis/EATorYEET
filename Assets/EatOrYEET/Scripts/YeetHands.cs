21d1c5e00c2279d24121f7b4fe6c80ec8795919f
Russell <russell@stitchmedia.ca>
2020-02-01 16:31:05 -0500

    Updated MainLevelScene with fixed hands

---
+++ Assets/EatOrYEET/Scripts/YeetHands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YeetHands : MonoBehaviour
+{
+    [SerializeField]
+    protected OVRInput.Controller m_controller;
+    
+    public float xVelocity = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.GetComponent<FoodItem>())
+        {   //FoodItem detected in collision
+            if (OVRInput.GetLocalControllerVelocity(m_controller).x > xVelocity)
+            {   // if the controller is moving faster than xVelocity then add that velocity to the food item
+                other.transform.position += OVRInput.GetLocalControllerVelocity(m_controller); 
+            }    
+        }
+
+    }
+}
