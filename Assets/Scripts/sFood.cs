b2feae35636e6064635411169b21a170f0409d98
TonyStitchMedia <tony@stitchmedia.ca>
2020-01-31 19:58:35 -0500

    Added sFood scriptable object

---
+++ Assets/Scripts/sFood.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Eat or YEET/Scriptable Objects/Food", fileName="New Food Type", order=51)]
+public class sFood : ScriptableObject
+{
+
+    [SerializeField] private List<GameObject> foodPrefabs = new List<GameObject>();
+    [SerializeField] private List<FoodCategory> foodCategories = new List<FoodCategory>();
+    [SerializeField] private string foodName = "New Food";
+    [SerializeField] private int pointValue = 0;
+    
+    enum FoodCategory
+    {
+        MEAT,
+        VEG,
+        FRUIT,
+        DAIRY,
+        CANDY,
+        DESSERT,
+        JUNKFOOD,
+        BREAD,
+        SEAFOOD,
+        INEDIBLE,
+    }
+
+}
