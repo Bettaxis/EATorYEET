ae6b605cb3fe165b983958cd8a27452faaa780f1
Adrian Miasik <AdrianMiasik@gmail.com>
2020-01-31 22:05:42 -0500

    Create a simple console log script

---
+++ Assets/ConsoleLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleLog : MonoBehaviour
+{
+    [SerializeField] private float displayForHowLongInSeconds = 3f;
+
+    private float accumulatedTime;
+    
+    private void Update()
+    {
+        accumulatedTime += Time.deltaTime;
+
+        if (accumulatedTime >= displayForHowLongInSeconds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
