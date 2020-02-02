d607a23875104ac5e4b140052f4de3799550b37f
Michael <michael@stitchmedia.ca>
2020-02-01 22:33:26 -0500

    extend-o-hands now lerp
    pretty sketchy, could be simplified

--- Assets/EatOrYEET/Scripts/cExtendOHands.cs
+++ Assets/EatOrYEET/Scripts/cExtendOHands.cs
@@ -9,33 +9,104 @@ using UnityEngine;
 public class cExtendOHands : MonoBehaviour
 {
     [SerializeField] private OVRInput.Controller controller;
-    [SerializeField] private Transform startPos; 
+    [SerializeField] private Transform backPos; 
     [SerializeField] private Transform forwardPos;
 
+    [SerializeField] private float armExtendSpeed;
     private bool isLerping;
+    private float lerpTimer;
+    private float lerpLength;
+
+    private Vector3 startPos;
+    private Vector3 targetPos;
+
+    private void Start()
+    {
+        startPos = backPos.position;
+        targetPos = forwardPos.position;
+
+        lerpLength = Vector3.Distance(backPos.position, forwardPos.position);
+
+        transform.position = backPos.position;
+    }
 
     void Update()
     {
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller))
-        {
-            MoveHandsForward();
-        }
-
-
         if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, controller))
         {
-            MoveHandsBack();
+            if (isLerping)
+            {
+                lerpTimer = 0;
+                startPos = transform.position;
+                targetPos = backPos.position;
+
+                lerpLength = Vector3.Distance(transform.position, backPos.position);
+
+            }
+
+            else
+            {
+                MoveHandsBack();
+            }
+            
         }
+
+        if (isLerping)
+        {
+            float distCovered = lerpTimer * armExtendSpeed;
+            float journeyFraction = distCovered / lerpLength;
+            transform.position = Vector3.Lerp(startPos, targetPos, journeyFraction);
+
+            lerpTimer += Time.deltaTime;
+
+            if(Mathf.Abs((transform.position - targetPos).magnitude) <= 0.01) //close enough to target
+            {
+                isLerping = false;
+
+            }
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller))
+        {
+            if (isLerping)
+            {
+                lerpTimer = 0;
+                startPos = transform.position;
+                targetPos = forwardPos.position;
+
+                lerpLength = Vector3.Distance(transform.position, forwardPos.position);
+
+            }
+            else
+            {
+                MoveHandsForward();
+            }
+
+            
+        }
+
+       
     }
 
     void MoveHandsForward()
     {
-        transform.position = forwardPos.position;
+        lerpTimer = 0;
+        isLerping = true;
+
+        startPos = backPos.position;
+        targetPos = forwardPos.position;
     }
     
     void MoveHandsBack()
     {
-        transform.position = startPos.position;
+        lerpTimer = 0;
+        isLerping = true;
+
+        startPos = forwardPos.position;
+        targetPos = backPos.position;
+
+        lerpLength = Vector3.Distance(forwardPos.position, backPos.position);
     }
 
 }
