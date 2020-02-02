ffd8b6fa7192a19669ff3e9792ee633fccc36d80
CAMELOT\cohung <colin.hung@genesys.com>
2020-02-02 16:04:25 -0500

    Basic stuff for resetting the game after it ends

---
+++ Assets/EatOrYEET/Scripts/ResetButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.GetComponent<cYeetHands>() != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
