88bd6da35a1ef029ab8a88b11694f098ff9a0ba6
Adrian Miasik <AdrianMiasik@gmail.com>
2020-02-02 18:46:30 -0500

    last boop

---
+++ Assets/screenshotpress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class screenshotpress : MonoBehaviour
+{
+    private void Reset()
+    {
+        ScreenCapture.CaptureScreenshot(Application.persistentDataPath + ".png", 3);
+     }
+
+    void OnMouseDown()
+    {
+        ScreenCapture.CaptureScreenshot(Application.persistentDataPath, 4);
+    }
+}
