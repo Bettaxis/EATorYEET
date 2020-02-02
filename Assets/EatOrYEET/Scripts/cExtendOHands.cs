d9c00828291d5cc7b767a802e8124109345ed4ed
Michael <michael@stitchmedia.ca>
2020-02-01 21:42:13 -0500

    Got extend-o-arms working, simple arm collider set up on right hand

---
+++ Assets/EatOrYEET/Scripts/cExtendOHands.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Shoots hand target forward on button press
+/// </summary>
+
+public class cExtendOHands : MonoBehaviour
+{
+    [SerializeField] private OVRInput.Controller controller;
+    [SerializeField] private Transform startPos; 
+    [SerializeField] private Transform forwardPos; 
+
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, controller))
+        {
+            MoveHandsForward();
+        }
+
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, controller))
+        {
+            MoveHandsBack();
+        }
+
+
+    }
+
+    void MoveHandsForward()
+    {
+        transform.position = forwardPos.position;
+    }
+    
+    void MoveHandsBack()
+    {
+        transform.position = startPos.position;
+    }
+
+}
