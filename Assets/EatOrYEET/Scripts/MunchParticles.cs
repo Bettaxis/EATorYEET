32f9bcb1ca7ea9cd9cb5feeb007a3b13579239c6
KelvinPut <kelvin@stitchmedia.ca>
2020-02-01 21:14:31 -0500

    created particle system prefab and script

---
+++ Assets/EatOrYEET/Scripts/MunchParticles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.ParticleSystemJobs;
+
+public class MunchParticles : MonoBehaviour
+{
+    [SerializeField] private Color particleColour;
+    private ParticleSystem munchParticleSystem;
+    void Start()
+    {
+        munchParticleSystem = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    /*
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            fireParticles();
+        }
+    }
+    */
+
+    public void fireParticles(){
+        munchParticleSystem.Play();
+    }
+}
