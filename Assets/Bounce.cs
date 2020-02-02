baddf7096d3769e9ce48fc6c9c576a901bcf1490
Humongor <humongor@gmail.com>
2020-02-02 17:55:13 -0500

    Added a boune pad for stuff to be thrown at
    throw shit at this and it will bounce

---
+++ Assets/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    [SerializeField] private float expStr;
+    [SerializeField] private float expRds;
+
+    private void OnTriggerEnter(Collider other)
+    {
+   
+        other.attachedRigidbody.AddExplosionForce(expStr, transform.position, expRds, 3f, ForceMode.Impulse);
+        Debug.Log("EXPLOSION");
+    }
+}
